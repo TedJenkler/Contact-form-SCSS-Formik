74bc46be1f3d500e5a061d2f97cb3b7b82c0e174
Ted Jenkler <tedjenkler@gmail.com>
2024-08-11 00:34:31 +0200

    added CustomCheckbox and button styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Formik, Field, Form } from "formik";
+import check from '../src/assets/images/check.png';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
           email: "",
           type: "",
           message: "",
-          toggle: false
+          toggle: false,
         }}
         onSubmit={async (values) => {
           await new Promise((r) => setTimeout(r, 500));
@@ -50,17 +51,27 @@ function App() {
           </label>
           <div id="radio-group">Query Type *</div>
           <div className="radio-group">
-  <label htmlFor="general-enquiry">
-    <input type="radio" id="general-enquiry" name="type" value="General Enquiry" />
-    <span className="custom-radio"></span>
-    General Enquiry
-  </label>
-  <label htmlFor="support-request">
-    <input type="radio" id="support-request" name="type" value="Support Request" />
-    <span className="custom-radio"></span>
-    Support Request
-  </label>
-</div>
+            <label htmlFor="general-enquiry">
+              <input
+                type="radio"
+                id="general-enquiry"
+                name="type"
+                value="General Enquiry"
+              />
+              <span className="custom-radio"></span>
+              General Enquiry
+            </label>
+            <label htmlFor="support-request">
+              <input
+                type="radio"
+                id="support-request"
+                name="type"
+                value="Support Request"
+              />
+              <span className="custom-radio"></span>
+              Support Request
+            </label>
+          </div>
           <label className="textfield" htmlFor="message">
             Message *
             <Field
@@ -70,11 +81,12 @@ function App() {
               placeholder="Hello, I would like to know if you're able to build Shopify e-commerce sites. We're starting a business and we're going to use Shopify. But it would be great to work with an agency who specialises in working with it."
             />
           </label>
-          <label className="toggle">
+          <label className="toggle-checkbox">
+            <Field type="checkbox" name="toggle" id="toggle" />
+            <span className="custom-checkbox"></span>
             I consent to being contacted by the team *
-            <Field type="checkbox" name="toggle" />
           </label>
-          <button type="submit">Submit</button>
+          <button className="submit" type="submit">Submit</button>
         </Form>
       </Formik>
     </>
--- src/components/RadioButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Field } from 'formik';
-import './RadioButton.css';
-
-const RadioButton = () => {
-
-}
-
-export default RadioButton;
